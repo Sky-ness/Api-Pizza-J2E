dd59ac70c438edd1801453b41c830d98f9db5f01
Antonin DUVAUCHEL <antonin.duvauchel.etu@hevea09.iut-infobio.priv.univ-lille1.fr>
2023-02-17 15:25:32 +0100

    connexion version 1

---
+++ WEB-INF/src/controleurs/ControleurLogin.java
@@ -0,0 +1,40 @@
+package controleurs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.UserDao;
+@WebServlet("/basicAuthentification")
+public class ControleurLogin extends HttpServlet{
+
+	/*
+	 * Page qui vérifie la connection
+	 */
+	
+	UserDao users;
+	
+	public void init(ServletConfig config) throws ServletException{
+		super.init(config);
+		users = new UserDao();
+		System.out.println("Démarrage de la servlet");
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		PrintWriter out = res.getWriter();
+		if(users.veriftoken(req)) {
+			String token = users.createToken(req);
+			out.println("Votre token est : "+token);
+		}
+		else {
+			res.sendError(401);
+		}
+		out.close();
+	}
+}
